7f53f482e259a0feb44053d2a95b7c49001b8bfb
Ahmad <ahmad.alsaleh@medcon-me.com>
2024-07-18 19:21:28 +0400

    .gitignore added

--- ios/Pods/Target Support Files/Pods-Parkology-ParkologyTests/Pods-Parkology-ParkologyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Parkology_ParkologyTests : NSObject
-@end
-@implementation PodsDummy_Pods_Parkology_ParkologyTests
-@end
--- src/components/widgets/avatar/index.tsx
+++ src/components/widgets/avatar/index.tsx
@@ -31,6 +31,9 @@ function ChooseAvatar() {
             const res = await post(routeConfig.avatar, {
                 avatar: avatar
             }, user?.token);
+            console.log('====================================');
+            console.log('res', res);
+            console.log('====================================');
             if (res && res.status_code == 200) {
                 storeUser(res.data)
                 dispatch(updateAvatar(avatar))
